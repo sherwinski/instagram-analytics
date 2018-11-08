dedc0b7f3235062633928d13d0573c8fde421fa8
ZackRhodes <30397274+ZackRhodes@users.noreply.github.com>
2018-11-07 23:27:50 -0500

    .gitignore is now working

--- config/auth.py
+++
@@ -1,6 +0,0 @@
-# Replace 'testUsername' with your account username or email
-username = 'testUsername'
-
-# Replace 'testPassword' with your account password
-password = 'testPassword'
-
--- config/path.py
+++
@@ -1,1 +0,0 @@
-driverPath = "/usr/local/bin/chromedriver"
